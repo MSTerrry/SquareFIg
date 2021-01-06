1472c69b4cb3c83669a71c90d64279e96599300a
Michael <mishatuhba@gmail.com>
2021-01-06 15:38:31 +0500

    Add project files.

---
+++ Square/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace Square
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.AddPointButton = new System.Windows.Forms.Button();
+            this.InputLabel = new System.Windows.Forms.Label();
+            this.InputPoint = new System.Windows.Forms.TextBox();
+            this.ResultTextbox = new System.Windows.Forms.TextBox();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.PointNumber = new System.Windows.Forms.Label();
+            this.RemovePointButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(222, 112);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(125, 34);
+            this.ClearButton.TabIndex = 0;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(222, 72);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(125, 34);
+            this.CalculateButton.TabIndex = 1;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // AddPointButton
+            // 
+            this.AddPointButton.Location = new System.Drawing.Point(222, 152);
+            this.AddPointButton.Name = "AddPointButton";
+            this.AddPointButton.Size = new System.Drawing.Size(125, 34);
+            this.AddPointButton.TabIndex = 2;
+            this.AddPointButton.Text = "Add point";
+            this.AddPointButton.UseVisualStyleBackColor = true;
+            this.AddPointButton.Click += new System.EventHandler(this.AddPointButton_Click);
+            // 
+            // InputLabel
+            // 
+            this.InputLabel.AutoSize = true;
+            this.InputLabel.Location = new System.Drawing.Point(25, 7);
+            this.InputLabel.Name = "InputLabel";
+            this.InputLabel.Size = new System.Drawing.Size(85, 20);
+            this.InputLabel.TabIndex = 4;
+            this.InputLabel.Text = "Input point";
+            // 
+            // InputPoint
+            // 
+            this.InputPoint.Location = new System.Drawing.Point(29, 30);
+            this.InputPoint.Name = "InputPoint";
+            this.InputPoint.Size = new System.Drawing.Size(150, 26);
+            this.InputPoint.TabIndex = 5;
+            // 
+            // ResultTextbox
+            // 
+            this.ResultTextbox.Location = new System.Drawing.Point(222, 30);
+            this.ResultTextbox.Name = "ResultTextbox";
+            this.ResultTextbox.Size = new System.Drawing.Size(164, 26);
+            this.ResultTextbox.TabIndex = 6;
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Location = new System.Drawing.Point(218, 7);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(55, 20);
+            this.ResultLabel.TabIndex = 7;
+            this.ResultLabel.Text = "Result";
+            // 
+            // PointNumber
+            // 
+            this.PointNumber.AutoSize = true;
+            this.PointNumber.Location = new System.Drawing.Point(1, 33);
+            this.PointNumber.Name = "PointNumber";
+            this.PointNumber.Size = new System.Drawing.Size(22, 20);
+            this.PointNumber.TabIndex = 8;
+            this.PointNumber.Text = "1.";
+            // 
+            // RemovePointButton
+            // 
+            this.RemovePointButton.Location = new System.Drawing.Point(222, 192);
+            this.RemovePointButton.Name = "RemovePointButton";
+            this.RemovePointButton.Size = new System.Drawing.Size(125, 34);
+            this.RemovePointButton.TabIndex = 9;
+            this.RemovePointButton.Text = "Remove Point";
+            this.RemovePointButton.UseVisualStyleBackColor = true;
+            this.RemovePointButton.Click += new System.EventHandler(this.RemovePointButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 359);
+            this.Controls.Add(this.RemovePointButton);
+            this.Controls.Add(this.PointNumber);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.ResultTextbox);
+            this.Controls.Add(this.InputPoint);
+            this.Controls.Add(this.InputLabel);
+            this.Controls.Add(this.AddPointButton);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.ClearButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Button AddPointButton;
+        private System.Windows.Forms.Label InputLabel;
+        private System.Windows.Forms.TextBox InputPoint;
+        private System.Windows.Forms.TextBox ResultTextbox;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Label PointNumber;
+        private System.Windows.Forms.Button RemovePointButton;
+    }
+}
+
---
+++ Square/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Square
+{
+    public partial class Form1 : Form
+    {
+        private int numberPoints = 1;
+        private string textboxName = "InputPoint";
+        public class Point
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+            public Point() { }
+            public Point(string[] str)
+            {
+                double x, y;
+                if (str.Length < 2) return;
+                var parseResultX = double.TryParse(str[0], out x);
+                var parseResultY = double.TryParse(str[1], out y);
+                if (!parseResultX)
+                    MessageBox.Show($"Input data was not correct. Error parse {str[0]}");
+                X = x;
+                if (!parseResultY)
+                    MessageBox.Show($"Input data was not correct. Error parse {str[1]}");
+                Y = y;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            Controls.Find(textboxName, true).Cast<TextBox>().ToList().ForEach(z => z.Clear());
+            ResultTextbox.Clear();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            var resultList = GetInputList();
+            var result = 0.0;
+            for (int i = 0; i < resultList.Count(); i++)
+            {
+                if (i + 1 != resultList.Count())
+                    result += resultList[i].X * resultList[i + 1].Y - resultList[i + 1].X * resultList[i].Y;
+                else
+                    result += resultList[i].X * resultList[0].Y - resultList[i].Y * resultList[0].X;
+            }
+            ResultTextbox.Text = (Math.Abs(result) / 2).ToString();
+        }
+        private List<Point> GetInputList()
+        {
+            var t = Controls.Find(textboxName, true).Cast<TextBox>();
+            var resultList = t.Select(z => MakePointFromText(z.Text)).ToList();
+            return resultList;
+        }
+        private Point MakePointFromText(string text)
+        {
+            return new Point(text.Replace('.', ',').Trim(' ').Replace(")", "").Replace("(", "").Split(';'));
+        }
+
+        private void AddPointButton_Click(object sender, EventArgs e)
+        {
+            var last = Controls.Find(textboxName, true).Last();
+            numberPoints++;
+
+            var label = new Label();
+            label.Text = $"{numberPoints}.";
+            label.Name = "PointNumber";
+            label.Location = new System.Drawing.Point(last.Location.X - 18, last.Location.Y + 29);
+
+            var textbox = new TextBox();
+            textbox.Size = new Size(100, 20);
+            textbox.Name = last.Name;
+            textbox.Location = new System.Drawing.Point(last.Location.X, last.Location.Y + 28);
+
+            Controls.Add(textbox);
+            Controls.Add(label);
+        }
+
+        private void RemovePointButton_Click(object sender, EventArgs e)
+        {
+            if (numberPoints == 1) return;
+            Controls.Remove(Controls.Find("InputPoint", true).Last());
+            Controls.Remove(Controls.Find("PointNumber", true).Last());
+            numberPoints--;
+        }
+    }
+}
